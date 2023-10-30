21f3645283ef138b81fec8899efd6cfb2b82bcca
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2023-10-31 01:44:10 +0530

    add searchbar

--- packages/blog-starter-kit/themes/vishalx360-blog/pages/index.tsx
+++ packages/blog-starter-kit/themes/vishalx360-blog/pages/index.tsx
@@ -30,6 +30,7 @@ import { ArrowLeft, LucideMoveRight } from 'lucide-react';
 import BackButton from '../components/BackButton';
 import Image from 'next/image';
 import { cn } from '../lib/utils';
+import { Search } from '../components/searchbar';
 
 const GQL_ENDPOINT = process.env.NEXT_PUBLIC_HASHNODE_GQL_ENDPOINT;
 
@@ -105,6 +106,9 @@ export default function Index({ publication, initialPosts, initialPageInfo }: Pr
 							<p className="mt-2 text-lg leading-8 text-gray-300">
 								Learn how to grow your business with our expert advice.
 							</p>
+							<div className='mt-5 z-30'>
+								<Search />
+							</div>
 						</div>
 						<StaggerParent className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 							{posts.map((post) => (
